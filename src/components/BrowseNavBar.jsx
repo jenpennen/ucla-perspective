6f02c0e9f82b25f1745191b2792311d52c4169f6
jenpennen <jennycho35@gmail.com>
2024-09-01 01:30:29 -0700

    installed font awesome package

--- src/components/BrowseNavBar.jsx
+++ src/components/BrowseNavBar.jsx
@@ -1,4 +1,8 @@
 import { Link } from "react-router-dom";
+// import "@fortawesome/fontawesome-free/css/all.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 function BrowseNavBar() {
   return (
     <div>
