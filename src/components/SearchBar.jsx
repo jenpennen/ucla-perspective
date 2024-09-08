0c08a4f5f7593c090aa6da188b1b761580ea8af6
jenpennen <jennycho35@gmail.com>
2024-09-08 16:34:59 -0700

    installed bootstrap and added searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,3 +1,24 @@
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+const SearchContainer = styled.div`
+  //display: flex;
+  border: 1px red solid;
+  align-items: center;
+  margin: 0.5rem 2rem 1rem 2rem;
+`;
+const SearchInput = styled.div`
+  padding: 0.5rem;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 5px;
+  font-size: 0.8rem;
+`;
 export function SearchBar() {
-  return <div>This is the Search bar</div>;
+  return (
+    <SearchContainer>
+      <SearchInput>
+        Search for club by name <Button variant="primary">Search</Button>
+      </SearchInput>
+    </SearchContainer>
+  );
 }
