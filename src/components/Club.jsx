a465a75435616a40118beb77df691c8938e127ad
nazcor <nascoron@gmail.com>
2024-09-12 00:39:11 -0700

    Created the club card component and added a sample of club listings on the landing page.

--- src/components/Club.jsx
+++ src/components/Club.jsx
@@ -1,11 +1,91 @@
 import React from "react";
-function Club() {
+import styled from "styled-components";
+
+const Tag = styled.button `
+  color: white;
+  text-transform: uppercase;
+  font-weight: bold;
+  width: fit-content;
+  background-color: purple;
+  margin-left: 25px;
+  padding: 6px;
+  border-radius: 5px;
+  border: none;
+  font-size: .7em;
+`;
+
+const ClubBox = styled.div`
+  background-color: white;
+  width: 50vh;
+  display: grid;
+  border-radius: 25px;
+  padding-top: 25px;
+  padding-bottom: 25px;
+  box-shadow: 0 0 5px #c2c4c3;
+  margin-bottom: 20px;
+`;
+
+const ClubName = styled.h3`
+  background-color: transparent;
+  justify-self: left;
+  padding-left: 25px;
+  padding-right: 25px;
+  margin: .5em 0;
+  font-weight: bold;
+  font-size: 1.3em;
+`;
+
+const Desc = styled.p`
+  background-color: transparent;
+  justify-self: left;
+  padding-left: 25px;
+  padding-right: 25px;
+  margin: 0;
+  font-weight: bold;
+  font-size: 1.1em;
+`;
+
+const Line = styled.hr `
+  width: 100%;
+  height: 1px;
+  opacity: 0.1;
+  background-color: grey;
+`;
+
+const More = styled.p`
+  background-color: transparent;
+  justify-self: left;
+  padding-left: 25px;
+  padding-right: 25px;
+  margin: 0;
+  font-style: italic;
+  font-weight: bold;
+  font-size: .9em;
+`;
+
+const Info = styled.p `
+  background-color: transparent;
+  margin-top: 0.5em;
+  text-align: left;
+  padding-left: 25px;
+  padding-right: 25px;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+  font-size: .9em;
+`;
+
+function Club(club) {
   return (
-    <div>
-      <h3>Club Name</h3>
-      <p>Club Info</p>
-      <p>Upcoming Events</p>
-    </div>
+    <ClubBox>
+      <Tag>{club.tag}</Tag>
+      <ClubName>{club.name}</ClubName>
+      <Desc>{club.desc}</Desc>
+      <Line/>
+      <More>More Info</More>
+      <Info>{club.info}</Info>
+    </ClubBox>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { SearchBar } from "../components/SearchBar";
 import BrowseNavBar from "../components/BrowseNavBar";
+import Club from "../components/Club";
 import "../App.css";
 
 function Home() {
@@ -16,6 +17,18 @@ function Home() {
       <div className="browse">
         <h2>Browse</h2>
         <BrowseNavBar />
+      </div>
+      <div className="directory">
+        <h2>Events Directory</h2>
+        <p>Click on any date to check out any cool events around!</p>
+        <div className="calendar">
+          This is a calendar.
+        </div>
+        <div className="club-listing">
+          <Club tag="Social" name="Disney Club" desc="Princess and the Frog Movie Night" info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, sequi dolorem? Illo, eaque. Quidem possimus neque nam voluptas sunt sed incidunt! Aut voluptatem neque facilis iure nam, error et magni. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam dignissimos ducimus voluptatibus. Molestiae enim animi corrupti ipsa doloribus labore suscipit magnam cumque, itaque, asperiores fuga beatae et facilis voluptatum esse!"/>
+          <Club tag="Career Development" name="UCLA DevX" desc="Boba Social at ShareTea" info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, minus modi. Earum cumque, eos in voluptatum ex omnis, nesciunt dolore doloribus rerum nobis voluptatibus temporibus iste quo iure ab. Repellat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat cupiditate labore nemo maiores voluptates architecto pariatur, repellat voluptatibus, earum ducimus quos illo similique! Dicta fugiat id sunt eius inventore provident."/>
+          <button class="more-button">View on More Events</button>
+        </div>
       </div>
     </body>
   );
