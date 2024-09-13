64cd219ff7641f23a8bc381c9773c06cc90fa2d9
nazcor <nascoron@gmail.com>
2024-09-12 17:32:54 -0700

    Added a title and filter buttons to the club directory.

--- src/pages/ClubDirectory.jsx
+++ src/pages/ClubDirectory.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Body = styled.body `
   display: flex;
-  justify-content: center;
+  flex-wrap: wrap;
 `;
 
 const Container = styled.div `
@@ -12,11 +12,36 @@ const Container = styled.div `
   flex-wrap: wrap;
   justify-content: center;
   gap: 2em;
+  margin-top: 2em;
+`;
+
+const Title = styled.h1 `
+  width: 100%;
+  font-weight: 800;
+  font-size: 2.3em;
+  text-align: left;
+  margin: 0;
+`;
+
+const Filters = styled.div `
+  margin: 0 25px;
+`;
+
+const Button = styled.button `
+  border: none;
+  text-transformation: uppercase;
+  font-size: 1.2em;
 `;
 
 function ClubDirectory() {
   return (
     <Body>
+      <Title>Club Directory</Title>
+      <Filters>
+        <Button>All</Button>
+        <Button>Recreational</Button>
+        <Button>Career Development</Button>
+      </Filters>
       <Container>
         <PropFull />
       </Container>
