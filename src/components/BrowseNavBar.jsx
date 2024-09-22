ff74b15b6944c6bef152be31ff5ede5b52a08864
jenpennen <jennycho35@gmail.com>
2024-09-21 17:17:19 -0700

    built out browse nav bar

--- src/components/BrowseNavBar.jsx
+++ src/components/BrowseNavBar.jsx
@@ -1,16 +1,37 @@
 import { Link } from "react-router-dom";
+import { FaFolder, FaCalendarAlt, FaHome } from "react-icons/fa";
+import styled from "styled-components";
+import "./NavBar.css";
 
+const IconContainer = styled.div`
+  display: block;
+`;
 function BrowseNavBar() {
   return (
-    <div>
+    <div className="icons-bar">
       <Link to="/ClubDirectory">
-        <button>Club Directory</button>
+        <button className="browse-button">
+          <div className="icon-container">
+            <FaFolder size={20} className="browse-icons" />
+          </div>
+          Club Directory
+        </button>
       </Link>
       <Link to="/Events">
-        <button>Events</button>
+        <button className="browse-button">
+          <div className="icon-container">
+            <FaCalendarAlt size={20} className="browse-icons" />
+          </div>
+          Events
+        </button>
       </Link>
       <Link to="/RegisterClub">
-        <button>Register Club</button>
+        <button className="browse-button">
+          <div className="icon-container">
+            <FaHome size={25} className="browse-icons" />
+          </div>{" "}
+          Register Club
+        </button>
       </Link>
       <div></div>
     </div>
